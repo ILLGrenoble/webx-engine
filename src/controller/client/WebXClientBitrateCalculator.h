d364e8dd3442edfb88a0c6219cc690071f7d5110
Stuart Caunt <caunt@ill.fr>
2025-03-10 08:35:19 +0000

    Calculating a rolling average RTT latency

--- src/controller/client/WebXClientBitrateCalculator.h
+++ src/controller/client/WebXClientBitrateCalculator.h
@@ -18,60 +18,97 @@ private:
         std::chrono::high_resolution_clock::time_point timestamp;
     };
 
+    class WebXClientLatencyData {
+    public:
+        WebXClientLatencyData(float rttLatencyMs) :
+            rttLatencyMs(rttLatencyMs),
+            timestamp(std::chrono::high_resolution_clock::now()) {}
+        virtual ~WebXClientLatencyData() {}
+
+        float rttLatencyMs;
+        std::chrono::high_resolution_clock::time_point timestamp;
+    };
+
 public:
-    WebXClientBitrateCalculator() {}
+    WebXClientBitrateCalculator() : _avgRTTLatencyMs(0.0) {}
     virtual ~WebXClientBitrateCalculator() {}
 
-
     void updateLatency(uint64_t sendTimestampMs, uint64_t recvTimestampMs) {
-        this->_rttLatencyMs = recvTimestampMs - sendTimestampMs;
-        spdlog::info("send time = {:d} recv time = {:d} RTT latency = {:d}", sendTimestampMs, recvTimestampMs, this->_rttLatencyMs);
+        float rttLatencyMs = recvTimestampMs - sendTimestampMs;
+        this->_latencyDataPoints.push_back(WebXClientLatencyData(rttLatencyMs));
+
+        this->calculateAverageRTTLatency();
+
+        spdlog::info("send time = {:d} recv time = {:d} avg RTT latency = {:f}", sendTimestampMs, recvTimestampMs, this->_avgRTTLatencyMs);
     }
 
     void updateBitrateData(uint64_t sendTimestampMs, uint64_t recvTimestampMs, uint32_t dataLength) {
-        uint64_t adjustedRecvTimestampMs = recvTimestampMs - this->_rttLatencyMs;
+        uint64_t adjustedRecvTimestampMs = recvTimestampMs - this->_avgRTTLatencyMs;
         if (adjustedRecvTimestampMs > sendTimestampMs) {
             uint32_t transferTimeMs = adjustedRecvTimestampMs - sendTimestampMs;
             float bitrateMbps = (0.00762939 * dataLength) / transferTimeMs; // (b * 8 / 1024 / 1024) / (ms / 1000)
             // spdlog::info("transfer time = {:d} for {:d} bytes => bitrate = {:f} Mb/s", transferTimeMs, dataLength, bitrateMbps);
 
-            this->_dataPoints.push_back(WebXClientBitrateData(bitrateMbps));
+            this->_bitrateDataPoints.push_back(WebXClientBitrateData(bitrateMbps));
         }
 
         this->calculateAverageBitrate();
-        if (this->_averageMbps.valid) {
-            spdlog::info("Average bitrate = {:f} Mb/s", this->_averageMbps.Mbps);
+        if (this->_avgBitrateMbps.valid) {
+            spdlog::info("Average bitrate = {:f} Mb/s", this->_avgBitrateMbps.Mbps);
         }
-    }
-
-    WebXDataRate calculateAverageBitrate() {
-        // Remove expired data
-        std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-        this->_dataPoints.erase(std::remove_if(this->_dataPoints.begin(), this->_dataPoints.end(), [&now](const WebXClientBitrateData & dataPoint) {
-            std::chrono::duration<double, std::milli> durationMs = now - dataPoint.timestamp;
-            return durationMs.count() > DATA_RETENTION_TIME_MS; 
-        }), this->_dataPoints.end());
-
-        if (this->_dataPoints.size() > 1) {
-            float totalBitrate = 0.0;
-            for (const WebXClientBitrateData & data : this->_dataPoints) {
-                totalBitrate += data.bitrateMbps;
-            }
-
-            this->_averageMbps = WebXDataRate(totalBitrate / this->_dataPoints.size());
-
-        } else {
-            this->_averageMbps = WebXDataRate();
-        }
-
-        return this->_averageMbps;
     }
 
 private:
-    const static int DATA_RETENTION_TIME_MS = 4000;
-    std::vector<WebXClientBitrateData> _dataPoints;
-    WebXDataRate _averageMbps;
-    uint32_t _rttLatencyMs; // Round-Trip Time
+    void calculateAverageRTTLatency() {
+
+        // Remove expired data
+        std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+        this->_latencyDataPoints.erase(std::remove_if(this->_latencyDataPoints.begin(), this->_latencyDataPoints.end(), [&now](const WebXClientLatencyData & dataPoint) {
+            std::chrono::duration<double, std::milli> durationMs = now - dataPoint.timestamp;
+            return durationMs.count() > LATENCY_DATA_RETENTION_TIME_MS; 
+        }), this->_latencyDataPoints.end());
+
+        if (this->_latencyDataPoints.size() > 0) {
+            float totalLatencyMs = 0.0;
+            for (const WebXClientLatencyData & data : this->_latencyDataPoints) {
+                totalLatencyMs += data.rttLatencyMs;
+            }
+
+            this->_avgRTTLatencyMs = totalLatencyMs / this->_latencyDataPoints.size();
+
+        } else {
+            this->_avgRTTLatencyMs = 0.0;
+        }
+    }
+
+    void calculateAverageBitrate() {
+        // Remove expired data
+        std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+        this->_bitrateDataPoints.erase(std::remove_if(this->_bitrateDataPoints.begin(), this->_bitrateDataPoints.end(), [&now](const WebXClientBitrateData & dataPoint) {
+            std::chrono::duration<double, std::milli> durationMs = now - dataPoint.timestamp;
+            return durationMs.count() > BITRATE_DATA_RETENTION_TIME_MS; 
+        }), this->_bitrateDataPoints.end());
+
+        if (this->_bitrateDataPoints.size() > 1) {
+            float totalBitrate = 0.0;
+            for (const WebXClientBitrateData & data : this->_bitrateDataPoints) {
+                totalBitrate += data.bitrateMbps;
+            }
+
+            this->_avgBitrateMbps = WebXDataRate(totalBitrate / this->_bitrateDataPoints.size());
+
+        } else {
+            this->_avgBitrateMbps = WebXDataRate();
+        }
+    }
+
+private:
+    const static int LATENCY_DATA_RETENTION_TIME_MS = 60000;
+    const static int BITRATE_DATA_RETENTION_TIME_MS = 4000;
+    std::vector<WebXClientBitrateData> _bitrateDataPoints;
+    std::vector<WebXClientLatencyData> _latencyDataPoints;
+    WebXDataRate _avgBitrateMbps;
+    float _avgRTTLatencyMs; // Round-Trip Time
 };
 
 
