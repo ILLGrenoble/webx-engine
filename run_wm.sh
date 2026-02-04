1ccecde08b775cda218f9160603892be8f1c8fe4
Stuart Caunt <caunt@ill.fr>
2026-02-04 14:25:09 +0000

    update run_wm.sh script to allow for use of Xvfb

--- run_wm.sh
+++ run_wm.sh
@@ -14,6 +14,7 @@ export XRDP_START_HEIGHT=1220
 
 FORCE_START=0
 DO_STOP=0
+X_PROCESS=Xorg
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -48,43 +49,72 @@ while [[ $# -gt 0 ]]; do
         shift
         ;;
 
+        -x|--xserver)
+        X_PROCESS="$2"
+        shift
+        shift
+        ;;
+
         *)
         break
         ;;
     esac
 done
 
-xorg_proc=`ps aux | grep Xorg | grep $DISPLAY | grep -v grep | awk '{print $2}'`
-if [ "$xorg_proc" != "" ]; then
-    if [ $DO_STOP == 1 ]; then
-        echo "Stopping Xorg running on display $DISPLAY"
+cleanup_x() {
+    echo "Cleaning up process $X_PROCESS on display $DISPLAY"
 
-        kill -9 $xorg_proc
-        exit 0
-    
-    elif [ $FORCE_START != 1 ]; then
-        echo "Xorg is already running on display $DISPLAY"
-        exit 1
+    x_proc=`ps aux | grep -v $0 | grep $X_PROCESS | grep $DISPLAY | grep -v grep | awk '{print $2}'`
+    if [ "$x_proc" != "" ]; then
+        if [ $DO_STOP == 1 ]; then
+            echo "Stopping $X_PROCESS running on display $DISPLAY"
 
-    elif [ $FORCE_START == 1 ]; then
-        echo "Xorg is already running on display $DISPLAY: stopping current process..."
-        kill -9 $xorg_proc
-        sleep 3
+            kill -9 $x_proc
+            exit 0
 
+        elif [ $FORCE_START != 1 ]; then
+            echo "$X_PROCESS is already running on display $DISPLAY"
+            exit 1
+
+        elif [ $FORCE_START == 1 ]; then
+            echo "$X_PROCESS is already running on display $DISPLAY: stopping current process..."
+            kill -9 $x_proc
+            sleep 3
+
+        fi
     fi
+}
 
-fi
+case "$X_PROCESS" in
+    Xorg|Xvfb)
+        ;;
+    *)
+        echo "Xserver $X_PROCESS is not known: must be either Xorg or Xvfb"
+        exit 1
+        ;;
+esac
+
+
+cleanup_x
 
 if [ $DO_STOP != 1 ]; then
-    echo "Starting Xorg on display $DISPLAY in the background (logs available at /tmp/xorg.log)..."
-    nohup Xorg :20 -config /etc/X11/xrdp/xorg.conf -verbose > /tmp/xorg.log 2>&1 &
+    echo "Starting $X_PROCESS on display $DISPLAY with resolution ${XRDP_START_WIDTH}x${XRDP_START_HEIGHT} in the background (logs available at /tmp/xorg.log)..."
+
+    case "$X_PROCESS" in
+        Xorg)
+            nohup Xorg $DISPLAY -config /etc/X11/xrdp/xorg.conf -verbose > /tmp/xorg.log 2>&1 &
+            ;;
+        Xvfb)
+            nohup Xvfb $DISPLAY -screen 0 ${XRDP_START_WIDTH}x${XRDP_START_HEIGHT}x24 > /tmp/xvfb.log 2>&1 &
+            ;;
+    esac
 
     sleep 3
 
     echo "... Starting xfce4"
     startxfce4
 
-elif [ "$xorg_proc" == "" ]; then
-    echo "Xorg is not running on display $DISPLAY"
+elif [ "$x_proc" == "" ]; then
+    echo "$X_PROCESS is not running on display $DISPLAY"
 fi
 
